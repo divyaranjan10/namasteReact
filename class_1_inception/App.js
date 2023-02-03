6b4d575a8ad7ab3925ab6379f466fef3967220c9
Divyaranjan Pradhan <divyaranjan619@gmail.com>
2023-02-03 22:10:17 +0530

    Class - Inception

---
+++ class_1_inception/App.js
@@ -0,0 +1,18 @@
+//every react element is an object
+//for ex: it takes the arguments -> h1, {}, "namaste everyone" and converts it into an object.
+const heading1 = React.createElement("h1",{
+    id:"title1",
+},"Heading 1");
+
+const heading2 = React.createElement("h2",{
+    id:"title2",
+},"Heading 2");
+
+const container = React.createElement("div",{
+    id:"container",
+}, [heading1, heading2])
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+//passing a react element inside the root
+root.render(container);
