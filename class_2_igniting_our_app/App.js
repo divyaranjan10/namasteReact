15f8f6f5b2fd8e6333db03fcb97ab8931920fbb3
Divyaranjan Pradhan <divyaranjan619@gmail.com>
2023-02-07 22:38:53 +0530

    Namaste React 3rd Class - Laying the Foundation

--- class_2_igniting_our_app/App.js
+++ class_2_igniting_our_app/App.js
@@ -3,35 +3,55 @@ import ReactDOM from "react-dom/client";
 
 const heading1 = React.createElement("h1",{
     id:"title1",
+    key:"h1"
 },"Heading 1");
 
 const heading2 = React.createElement("h2",{
     id:"title2",
+    key:"h2"
 },"Heading 2");
+
+//React.createElment => Object => HTML Code(DOM)
 
 const container = React.createElement("div",{
     id:"container",
     hello:"world"
 }, [heading1, heading2])
 
+//JSX -> JSX is a HTML like syntax inside Javascript
+//Advantages of JSX -> Readability, Less Code
+//Babel is a JS compiler/package that converts jsx to html
+
+//Components in React
+//1. Functional Component -> It's basically a JS function that return some piece of JSX or a react element
+//2. Class based component
+//** Name of component starts with capital letter. */
+
+
+const line = (
+    <h1 id="line1" key="jsxLine">Hello! it's JSX</h1>
+);
+    
+const HeaderComponent = () => {
+    return (
+        <div>
+            {Try()}
+            <h1>Hello! It's a functional component, Heading 1.</h1>
+            <h2>Hello! It's a functional component, heading 2.</h2>
+        </div>
+    )
+};
+
+const Try = () => (
+    <h1 id="line1" key="jsxLine">Using functional component inside another component.</h1>
+);
+
+
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 //passing a react element inside the root
-root.render(container);
-
-
-//production ready -> minify the code, optimize, clean console and bundle
-//Bundlers --> webpack, vite, parcel --> package ---> to use it, we need --> package manager(npm) --> no it's not called Node Package Manager XD
-//why do we need NPM? --> because we need to manage a lot of packages in our code
-//what are dependencies? --> so basically dependencies are all the packages that our project needs.
-//so, when you install something, node modules gets installed into your project
-//so, basically node modules is kind of database for your npm
-//node modules are the heaviest object in the universe (jk)
-//we do not need to push node modules into our git repositories
-//because we have 'package-lock.json' and 'package.json' with us, that sufficient info to create node modules for us
-//npx --> execute using npm
-//dist folder -> it keeps the files minifies for us
-//browserlist package
+root.render(<HeaderComponent/>);
 
 // *    power of parcel
 // *    - HMR : Hot Module Reloading (basically parcel will take care of all the files in your directories)
@@ -48,11 +68,7 @@ root.render(container);
 // *    - HTTPS on dev
 // *    - uses port number
 // *    - Consistent Hashing
-
-// Transitive Dependencies -> we have our package manager which handles and takes care of our transitive dependencies in our code.
-
-
-//So, we are changing content and it automatically changing the content
-//this thing is called Hot MOdule Reload - this power is given by parcel
-//File Watcher Algorithm - C++
-//npx parcel index.html(entry point) --> it's just create a develoepment build for us an d host it on our server
+// *    - Tree Shaking (Removing Unwanted Code)
+// *    - **Scenario for Tree Shaking** - Suppose our app is importing a library, that containd 10 helper functions and we just need 2-3 
+// *    - functions in our project, so 'parcel' will ignore all the unwanted code(functions) not required in our project 
+// *    - Zero Config 
