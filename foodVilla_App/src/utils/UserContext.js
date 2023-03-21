e95c4c4761cb1cf504d9e880dada4719bc3165e9
Divyaranjan Pradhan <divyaranjan619@gmail.com>
2023-03-21 12:49:30 +0530

    Updated - Chapter 11 - Data is the new Oil

--- foodVilla_App/src/utils/UserContext.js
+++ foodVilla_App/src/utils/UserContext.js
@@ -6,4 +6,7 @@ const UserContext = createContext({
         email: "dummy@gmail.com"
 }});
 
+//suppose there are 10 context that you are using, so you can give it a default name for debugging purposes.
+UserContext.displayName = "UserContext";
+
 export default UserContext;
